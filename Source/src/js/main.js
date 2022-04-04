4f4a8b0cfad295bff844eff04cfeb97d8d4ba137
Anton Grishin <grishin.ant.a@gmail.com>
2022-04-04 22:35:38 +0500

    Добавлено динамическое создание modal

--- Source/src/js/main.js
+++ Source/src/js/main.js
@@ -1,15 +1,38 @@
 import "./lib/lib";
 
-$("#first").on("click", () => {
-  $("div").eq(1).fadeToggle(800);
-});
+// $("#first").on("click", () => {
+//   $("div").eq(1).fadeToggle(800);
+// });
 
-$("[data-count='second']").on("click", () => {
-  $("div").eq(2).fadeToggle(800);
-});
+// $("[data-count='second']").on("click", () => {
+//   $("div").eq(2).fadeToggle(800);
+// });
 
-$("button")
-  .eq(2)
-  .on("click", () => {
-    $(".w-500").fadeToggle(800);
-  });
+// $("button")
+//   .eq(2)
+//   .on("click", () => {
+//     $(".w-500").fadeToggle(800);
+//   });
+
+$("#trigger").click(() =>
+  $("#trigger").createModal({
+    text: {
+      title: "Modal Title",
+      body: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+    },
+    btns: {
+      count: 2,
+      settings: [
+        ["Close", ["btn-danger", "mr-10"], true],
+        [
+          "Save changes",
+          ["btn-success"],
+          false,
+          () => {
+            alert("Данные сохранены");
+          },
+        ],
+      ],
+    },
+  })
+);
